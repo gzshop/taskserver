c510ef2c131b7e1c17dcc8ab6bc450960be1cc38
Tc <jackgoodchen2019@gmail.com>
2020-06-22 12:10:39 +0800

    task_init

--- apps/task/task.py
+++ apps/task/task.py
@@ -2,16 +2,20 @@
 
 
 from models.task import Order
+from loguru import logger
 
 def order_handler(**kwargs):
 
     db = kwargs.get("db")
     orderid = kwargs.get("orderid")
 
+    logger.info("订单号{}".format(orderid))
     with db.atomic():
         orders = Order.select().where(Order.orderid == orderid).for_update()
         for order in orders:
             if order.status == '0':
                 order.status ='9'
                 order.save()
+
+                logger.info("订单号{}处理成功!".format(orderid))
 
